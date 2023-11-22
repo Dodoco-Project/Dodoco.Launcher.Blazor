b07d9900c5cc2883f3e16d3ad3bb27bde87700be
Brendo Costa <brendocosta@id.uff.br>
2023-11-22 03:07:16 -0300

    Added components

--- Source/Dodoco.Launcher.Blazor/index.js
+++ Source/Dodoco.Launcher.Blazor/index.js
@@ -18,37 +18,27 @@ let dodocoLaunchertheme = {
     },
     fontFamily: {
         "sans": ["\"Josefin Sans\""]
-    },
-    textShadow: {
-        "sm": "0 0 2px var(--tw-shadow-color)",
-        "md": "0 0 4px var(--tw-shadow-color)",
-        "DEFAULT": "0 0 4px var(--tw-shadow-color)",
-        "lg": "0 0 16px var(--tw-shadow-color)",
-        "xl": "0 0 24px var(--tw-shadow-color)",
-        "2xl": "0 0 48px var(--tw-shadow-color)",
     }
 };
 
 install(
     defineConfig({
         preflight: {
-            "@import": ["url(\"../Font/Josefin_Sans/JosefinSans.css\")"],
+            "@import": "url(\"../Font/Josefin_Sans/JosefinSans.css\")",
             "*:not(input[type=\"text\"])": {
                 "caret-color": "transparent"
             },
             "::-webkit-scrollbar": {
-                "width": "10px",
+                "@apply": "w-2"
             },
             "::-webkit-scrollbar-track": {
-                "background": dodocoLaunchertheme.colors.background,
-                "border-radius": dodocoLaunchertheme.borderRadius.DEFAULT
+                "@apply": "bg-transparent"
             },
             "::-webkit-scrollbar-thumb": {
-                "background": "rgba(0, 0, 0, 0.2)",
-                "border-radius": dodocoLaunchertheme.borderRadius.DEFAULT
+                "@apply": "bg-defaulttext/10 rounded"
             },
             "::-webkit-scrollbar-thumb:hover": {
-                "background": dodocoLaunchertheme.colors.primary
+                "@apply": "bg-primary"
             }
         },
         presets: [ presetAutoprefix(), presetTailwind() ],
